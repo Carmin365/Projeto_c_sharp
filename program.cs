52272ff9afad7a51c92217e65f00cfb2f4f94ffa
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-04-23 16:24:41 -0300

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,38 @@
+using System;
+class figura1
+{
+    public struct Circulo1
+    {
+    private int  x,y;
+    private float diametro1;
+    public Circulo1(int a, int b, float Diametro1)
+    {
+    x = a;
+    y = b;
+    diametro1 = Diametro1
+    }
+}
+
+public struct Elipse1
+{
+}
+public void teste1()
+{
+    Circulo1 c_um = new Circulo1(0,0,10);
+    Console.WriteLine("Estrutura do tipo aninhada");
+        
+}
+
+}
+class CsharpApp
+{
+    static void Main()
+    {
+        figura1 fig_um = figura1();
+        fig_um.teste();
+        Console.ReadLine();
+        }
+    }
+
+
+
